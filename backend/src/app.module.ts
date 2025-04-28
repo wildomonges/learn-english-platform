ea16af7e24247e1292ac290caa54c9d2ff5571ea
Wildo Monges <wmonges@Wildos-MacBook-Pro.local>
2025-04-28 10:30:12 -0300

    Remove db config for now

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,17 +9,17 @@ import { SpeakingModule } from './modules/speaking/speaking.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT ?? '5432'),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DATABASE_HOST,
+    //   port: parseInt(process.env.DATABASE_PORT ?? '5432'),
+    //   username: process.env.DATABASE_USERNAME,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database: process.env.DATABASE_NAME,
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   entities: ['dist/**/*.entity{.ts,.js}'],
+    // }),
     TopicsModule,
     SpeakingModule
   ],
