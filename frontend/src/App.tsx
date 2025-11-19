554b6f6d7fd07bdc34e2dbe728ee55adbf24e617
ngortiz <nidigortiz21@gmail.com>
2025-11-19 14:31:57 -0300

    A new UserDetailsPage file is added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import AdminDashboard from './pages/Admin/AdminDashboard';
 
 import './App.css';
 import AdminLogin from './pages/Admin/AdminLogin';
+import UserDetailsPage from './pages/Admin/UserDetailsPage';
 
 const AppContent: React.FC = () => {
   return (
@@ -32,6 +33,7 @@ const AppContent: React.FC = () => {
 
         {/* Ruta por defecto si no encuentra ninguna */}
         <Route path='*' element={<Navigate to='/' />} />
+        <Route path='/admin/users/:userId' element={<UserDetailsPage />} />
       </Routes>
     </>
   );
